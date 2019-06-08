32ba94091b42025846c44036785241c545bc0bb0
Max Kramer <max@maxkramer.co>
2019-06-08 16:27:01 +0100

    Adds missing h1 tag

--- src/js/header/Header.js
+++ src/js/header/Header.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
-import Headline from 'grommet/components/Headline';
-import {Box} from "grommet";
+import {Box, Headline, Heading} from "grommet";
 import SocialButtonGroup from "../header/SocialButtonGroup";
 
 class Header extends Component {
   render() {
     return (
       <Box direction={"column"}>
-        <Headline>Hello. My name is Max Kramer.<br/></Headline>
+        <Heading>Hello. My name is Max Kramer.<br/></Heading>
         <Headline id={"subtitle"} size={"small"}>I’m an Engineering Lead at BCG Digital Ventures,
           Berlin.</Headline>
         <SocialButtonGroup/>
