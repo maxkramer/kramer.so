15f985a3aa2648504837880cae260166db1bb97a
Max Kramer <max@maxkramer.co>
2020-10-23 00:25:59 +0200

    fix: uses smaller avatar image

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import {Box, Heading, Image} from 'grommet';
 import SocialButtonGroup from './SocialButtonGroup';
-import Max from '../Max.webp'
+import Max from '../Max-small.webp'
 import styles from "../App.module.scss"
 
 class Header extends Component {
     render() {
         return (
             <Box alignSelf={'center'} direction={'column'}>
-                <Image alignSelf={'center'} className={styles.avatar} src={Max} loading={'lazy'} />
+                <Image alignSelf={'center'} className={styles.avatar} src={Max} loading={'lazy'} a11yTitle={"Picture of Max Kramer"} />
                 <SocialButtonGroup alignSelf={'center'} margin={{top: 'small'}} />
                 <Box direction={'column'} margin={{top: 'medium'}}>
-                    <Heading level={1} margin={{top: 'none', bottom: 'small'}} size={'medium'} a11yTitle={''}>Hello. My name is Max Kramer.</Heading>
+                    <Heading level={1} margin={{top: 'none', bottom: 'small'}} size={'medium'}>Hello. My name is Max Kramer.</Heading>
                     <Heading level={2} margin={{ vertical: 'none'}} size={'small'}>I’m an Interim-CTO at BCG Digital Ventures, Berlin.</Heading>
                 </Box>
             </Box>
